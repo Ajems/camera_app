5551b0866bca45d57a46bd69d5d10712c9c16a12
Ajems <70469206+Ajems@users.noreply.github.com>
2022-02-17 15:49:55 +0300

    Update AppViewModel.kt

--- app/src/main/java/com/example/cameraapp/AppViewModel.kt
+++ app/src/main/java/com/example/cameraapp/AppViewModel.kt
@@ -6,13 +6,9 @@ class AppViewModel: ViewModel() {
 
     var cntPhotos = 0
     var readyForPhoto = false
-    val FILE_NAME_FORMAT = "yy-MM-dd-HH-mm-ss-SSS"
+    val FILE_NAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
     var gridVisible = true
 
-    companion object{
-        private const val CAMERA_PERMISSON_CODE = 1
-        private const val CAMERA_REQUEST_CODE = 2
-    }
 
     fun addCntPhoto(): Int{
         cntPhotos ++
