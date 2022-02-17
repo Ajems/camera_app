8dd68c6a75d0113f3f182ff2efd0e346b2e07ee5
Ajems <70469206+Ajems@users.noreply.github.com>
2022-02-17 15:48:48 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/cameraapp/MainActivity.kt
+++ app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -1,24 +1,21 @@
 package com.example.cameraapp
 
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.core.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.cameraapp.databinding.ActivityMainBinding
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
-import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,7 +34,6 @@ class MainActivity : AppCompatActivity() {
 
         outputDirectory = getOutputDirectory()
         binding.cntPhotosView.text = appViewModel.cntPhotos.toString()
-
 
         //ask permission
         val requestCameraPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
@@ -73,41 +69,26 @@ class MainActivity : AppCompatActivity() {
             appViewModel.gridVisible = !appViewModel.gridVisible
         }
 
-        //заполнение activity
-        if (appViewModel.readyForPhoto){
-            startCamera()
-        }
-
         //get permission
+        //заполнение activity
         if (!appViewModel.readyForPhoto) requestCameraPermission.launch(android.Manifest.permission.CAMERA)
+        else startCamera()
 
-    }
-
-    private fun getOutputDirectory(): File{
-        val mediaDir = externalMediaDirs.firstOrNull()?.let{mFile ->
-            File(mFile, resources.getString(R.string.app_name)).apply {
-                mkdirs()
-            }
-        }
-
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else filesDir
     }
 
     private fun startCamera(){
 
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-        val cameraProvider:ProcessCameraProvider = cameraProviderFuture.get()
+        val cameraProvider = cameraProviderFuture.get()
 
         cameraProviderFuture.addListener({
             val preview = Preview.Builder()
                 .build()
                 .also {
-                    mPreview->mPreview.setSurfaceProvider(binding.camera.surfaceProvider)
+                    it.setSurfaceProvider(binding.camera.surfaceProvider)
                 }
 
-            imageCapture = ImageCapture.Builder()
-                .build()
+            imageCapture = ImageCapture.Builder().build()
 
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
@@ -116,12 +97,22 @@ class MainActivity : AppCompatActivity() {
                 cameraProvider.bindToLifecycle(
                     this, cameraSelector, preview, imageCapture
                 )
-
             }catch (e:Exception){
                 Toast.makeText(applicationContext, "Error open camera", Toast.LENGTH_SHORT).show()
             }
         }, ContextCompat.getMainExecutor(this))
 
+    }
+
+    private fun getOutputDirectory(): File{
+        val mediaDir = externalMediaDirs.firstOrNull()?.let{
+            File(it, resources.getString(R.string.app_name)).apply {
+                mkdirs()
+            }
+        }
+
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
     }
 
     fun savePhoto(){
@@ -134,15 +125,18 @@ class MainActivity : AppCompatActivity() {
         val outputOption = ImageCapture.OutputFileOptions.Builder(file).build()
 
         imageCapture.takePicture(
-            outputOption, ContextCompat.getMainExecutor(this),
+            outputOption,
+            ContextCompat.getMainExecutor(this),
             object : ImageCapture.OnImageSavedCallback{
                 override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+
                     val savedUri = Uri.fromFile(file)
 
                     Toast.makeText(
                         this@MainActivity,
                         "Photo saved to $savedUri",
                         Toast.LENGTH_SHORT).show()
+
                 }
 
                 override fun onError(exception: ImageCaptureException) {
@@ -151,6 +145,8 @@ class MainActivity : AppCompatActivity() {
                         "Error save photo",
                         Toast.LENGTH_SHORT).show()
                 }
+
+
             }
         )
     }
